a6e8603b084e0c478d66869cd489991fa95eb8c4
Olivia <oliviabelu@web.de>
2026-05-27 14:39:25 +0200

    added basic form structure with all fields

---
+++ components/PlantForm/index.js
@@ -0,0 +1,110 @@
+import useSWR from "swr";
+import styled from "styled-components";
+
+export default function PlantForm() {
+  const { mutate } = useSWR(`/api/plants`);
+
+  function handleSubmit() {}
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      aria-labelledby="Expand your Garden"
+      aria-describedby="add a new plant"
+    >
+      <fieldset>
+        <legend>Expand your Garden</legend>
+
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="botanicalName">Botanical Name: </label>
+        <input type="text" id="botanicalName" name="botanicalName" required />
+
+        <fieldset>
+          <legend>Light needs</legend>
+          <label htmlFor="lightNeed">Full Shade</label>
+          <input
+            type="radio"
+            id="lightNeed"
+            name="lightNeed"
+            value="Full Shade"
+            required
+          />
+          <label htmlFor="lightNeed">Partial Shade</label>
+          <input
+            type="radio"
+            id="lightNeed"
+            name="lightNeed"
+            value="Partial Shade"
+          />
+          <label htmlFor="lightNeed">Full Sun</label>
+          <input
+            type="radio"
+            id="lightNeed"
+            name="lightNeed"
+            value="Full Sun"
+          />
+        </fieldset>
+
+        <fieldset>
+          <legend>Water needs</legend>
+          <label htmlFor="waterNeed">Low</label>
+          <input
+            type="radio"
+            id="waterNeed"
+            name="waterNeed"
+            value="Low"
+            required
+          />
+          <label htmlFor="waterNeed">Medium</label>
+          <input type="radio" id="waterNeed" name="waterNeed" value="Medium" />
+          <label htmlFor="waterNeed">High</label>
+          <input type="radio" id="waterNeed" name="waterNeed" value="High" />
+        </fieldset>
+
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          size={300}
+        ></input>
+
+        <fieldset>
+          <legend>Fertiliser Season</legend>
+          <label htmlFor="fertiliserSeason">Spring</label>
+          <input
+            type="checkbox"
+            id="fertiliserSeason"
+            name="fertiliserSeason"
+            value={"Spring"}
+          ></input>
+          <label htmlFor="fertiliserSeason">Summer</label>
+          <input
+            type="checkbox"
+            id="fertiliserSeason"
+            name="fertiliserSeason"
+            value={"Summer"}
+          ></input>
+          <label htmlFor="fertiliserSeason">Fall</label>
+          <input
+            type="checkbox"
+            id="fertiliserSeason"
+            name="fertiliserSeason"
+            value={"Fall"}
+          ></input>
+          <label htmlFor="fertiliserSeason">Winter</label>
+          <input
+            type="checkbox"
+            id="fertiliserSeason"
+            name="fertiliserSeason"
+            value={"Winter"}
+          ></input>
+        </fieldset>
+
+        <button type="submit">plant your plant</button>
+      </fieldset>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import PlantForm from "@/components/PlantForm";
+
 export default function HomePage({ plants }) {
   return (
     <div>
       <h1>Welcome to greenary🌱</h1>
+      <PlantForm />
     </div>
   );
 }
