2629828b98106174edb8cbfdd034af17f406ae6f
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-28 10:59:00 +0200

    renaming data

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import PlantList from "@/components/PlantList";
 import useSWR from "swr";
 
 export default function HomePage() {
-  const { data, isLoading } = useSWR("/api/plants");
+  const { data: plants, isLoading } = useSWR("/api/plants");
 
   if (isLoading) return <p>Loading your garden...</p>;
 
   return (
     <div>
       <h1>Greenary 🌱</h1>
-      <PlantList plants={data} />
+      <PlantList plants={plants} />
     </div>
   );
 }
