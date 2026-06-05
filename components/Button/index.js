4a5c5def00b21ee66dde4f33c1bb4a499d59daf8
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-06-05 14:24:08 +0200

    adding button component, and first styling test

---
+++ components/Button/index.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export default styled.button`
+  background-color: var(--secondary-off-white);
+  color: var(--primary-green-400);
+  border: 1px solid var(--primary-green-400);
+  border-radius: 4px;
+  padding: 16px 8px;
+  font-size: 1rem;
+  letter-spacing: 0.08em;
+`;
