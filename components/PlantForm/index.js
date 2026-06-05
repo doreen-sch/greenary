69dfba09a867ac416a9711a0d2513ea0b5a3d30c
Olivia <oliviabelu@web.de>
2026-06-05 14:28:44 +0200

    cleaned up a bit

--- components/PlantForm/index.js
+++ components/PlantForm/index.js
@@ -8,9 +8,7 @@ export default function PlantForm({
   onSubmit,
   onClearPlant,
   onCancelEdit,
-  // onShowEditForm,
 }) {
-  console.log("form water need", plant);
   return (
     <StyledForm
       onSubmit={onSubmit}
@@ -63,93 +61,6 @@ export default function PlantForm({
             plant={plant}
           ></PlantNeeds> */}
         </StyledFieldset>
-
-        <fieldset>
-          <legend>Light needs</legend>
-
-          <StyledInput
-            type="radio"
-            id="lightNeed-fullSun"
-            name="lightNeed"
-            value="Full Sun"
-            defaultChecked={plant?.lightNeed === "Full Sun"}
-            onChange={onSetPlantForm}
-            required
-          />
-          <StyledLabel htmlFor="lightNeed-fullSun">Full Sun</StyledLabel>
-
-          <StyledInput
-            type="radio"
-            id="lightNeed-partialShade"
-            name="lightNeed"
-            value="Partial Shade"
-            defaultChecked={plant?.lightNeed === "Partial Shade"}
-            onChange={onSetPlantForm}
-          />
-          <StyledLabel htmlFor="lightNeed-partialShade">
-            Partial Shade
-          </StyledLabel>
-
-          <StyledInput
-            type="radio"
-            id="lightNeed-fullShade"
-            name="lightNeed"
-            value="Full Shade"
-            defaultChecked={plant?.lightNeed === "Full Shade"}
-            onChange={onSetPlantForm}
-          />
-          <StyledLabel htmlFor="lightNeed-fullShade">Full Shade</StyledLabel>
-        </fieldset>
-
-        {/* <fieldset>
-          <legend>Water needs</legend>
-
-          <StyledInput
-            type="radio"
-            id="waterNeed-low"
-            name="waterNeed"
-            value="Low"
-            defaultChecked={plant?.waterNeed === "Low"}
-            onChange={onSetPlantForm}
-            required
-          />
-          <StyledLabel htmlFor="waterNeed-low">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-droplet-icon lucide-droplet"
-            >
-              <path d="M12 22a7 7 0 0 0 7-7c0-2-1-3.9-3-5.5s-3.5-4-4-6.5c-.5 2.5-2 4.9-4 6.5C6 11.1 5 13 5 15a7 7 0 0 0 7 7z" />
-            </svg>
-          </StyledLabel>
-
-          <StyledInput
-            type="radio"
-            id="waterNeed-medium"
-            name="waterNeed"
-            value="Medium"
-            defaultChecked={plant?.waterNeed === "Medium"}
-            onChange={onSetPlantForm}
-          />
-          <StyledLabel htmlFor="waterNeed-medium">Medium</StyledLabel>
-
-          <StyledInput
-            type="radio"
-            id="waterNeed-high"
-            name="waterNeed"
-            value="High"
-            defaultChecked={plant?.waterNeed === "High"}
-            onChange={onSetPlantForm}
-          />
-          <StyledLabel htmlFor="waterNeed-high">High</StyledLabel>
-        </fieldset> */}
 
         <fieldset>
           <legend>Fertiliser Season</legend>
--- components/PlantNeeds/index.js
+++ components/PlantNeeds/index.js
@@ -1,48 +1,4 @@
-import { Droplet, Sun } from "lucide-react";
-import Image from "next/image";
 import styled from "styled-components";
-
-// const sunIcon = (
-//   <svg
-//     xmlns="http://www.w3.org/2000/svg"
-//     width="24"
-//     height="24"
-//     viewBox="0 0 24 24"
-//     fill="none"
-//     stroke="currentColor"
-//     strokeWidth="2"
-//     strokeLinecap="round"
-//     strokeLinejoin="round"
-//     className="lucide lucide-sun-icon lucide-sun"
-//   >
-//     <circle cx="12" cy="12" r="4" />
-//     <path d="M12 2v2" />
-//     <path d="M12 20v2" />
-//     <path d="m4.93 4.93 1.41 1.41" />
-//     <path d="m17.66 17.66 1.41 1.41" />
-//     <path d="M2 12h2" />
-//     <path d="M20 12h2" />
-//     <path d="m6.34 17.66-1.41 1.41" />
-//     <path d="m19.07 4.93-1.41 1.41" />
-//   </svg>
-// );
-
-// const dropletIcon = (
-//   <svg
-//     xmlns="http://www.w3.org/2000/svg"
-//     width="24"
-//     height="24"
-//     viewBox="0 0 24 24"
-//     fill="none"
-//     stroke="currentColor"
-//     strokeWidth="2"
-//     strokeLinecap="round"
-//     strokeLinejoin="round"
-//     className="lucide lucide-droplet-icon lucide-droplet"
-//   >
-//     <path d="M12 22a7 7 0 0 0 7-7c0-2-1-3.9-3-5.5s-3.5-4-4-6.5c-.5 2.5-2 4.9-4 6.5C6 11.1 5 13 5 15a7 7 0 0 0 7 7z" />
-//   </svg>
-// );
 
 const plantNeeds = [
   {
@@ -108,9 +64,6 @@ export default function PlantNeeds({ plant, need, onSetPlantForm }) {
   const plantNeed = plantNeeds.find((plantNeed) => plantNeed.id === need);
 
   const numberOfNeed = plantNeed.need.indexOf(currentValue) + 1;
-  console.log("currentValue", currentValue);
-  console.log("plantNeed", plantNeed);
-  console.log("number of need", numberOfNeed);
 
   function toCamelCase(str) {
     return str
@@ -126,56 +79,31 @@ export default function PlantNeeds({ plant, need, onSetPlantForm }) {
       .join("");
   }
 
-  function getIcon(icon) {
-    switch (icon) {
-      case "droplet":
-        dropletIcon;
-        break;
-      case "sun":
-        sunIcon;
-        break;
-    }
-  }
-
-  function showNeeds(number) {
+  function showNeeds() {
     let returnComponent = [];
     for (let i = 1; i <= plantNeed.need.length; i++) {
       const value = plantNeed.need[i - 1];
-      console.log(i, numberOfNeed);
+      const technicalValue = need.concat("-", toCamelCase(value));
+
       returnComponent.push(
         <StyledLabel
-          key={need.concat("-", toCamelCase(value))}
-          htmlFor={need.concat("-", toCamelCase(value))}
+          key={technicalValue}
+          htmlFor={technicalValue}
           $needIndex={i}
           $numberOfNeed={numberOfNeed}
         >
           {plantNeed.icon}
-          {/* {getIcon(need.icon)} */}
 
           <StyledInput
             type="radio"
-            id={need.concat("-", toCamelCase(value))}
+            id={technicalValue}
             name={need}
             value={value}
             defaultChecked={plant?.need === value}
             onChange={onSetPlantForm}
           />
         </StyledLabel>
-        // <StyledListItem key={i}>
-        //   <StyledButton
-        //     alt=""
-        //     // type="button"
-        //     $needIndex={i}
-        //     $numberOfNeed={numberOfNeed}
-        //     onClick={handleNeed}
-        //   >
-        //     {dropletIcon}
-        //   </StyledButton>
-        // </StyledListItem>
       );
-      // return (
-      //    <span>{i <= number ? <Image alt="" onClick={}/> : <Droplet opacity={0.2} />}</span>
-      // );
     }
     console.log(returnComponent);
     return returnComponent;
@@ -185,30 +113,13 @@ export default function PlantNeeds({ plant, need, onSetPlantForm }) {
     <StyledWrapper>
       <p>{plantNeed.name}</p>
       <>{showNeeds()}</>
-      {/* <StyledList>{showNeeds()}</StyledList> */}
-      {/* <StyledSpan $test="">{dropletIcon}</StyledSpan>
-      <StyledSpan>{sunIcon}</StyledSpan> */}
     </StyledWrapper>
   );
 }
 
-const StyledSpan = styled.span`
-  opacity: 0.2;
-`;
-
 const StyledWrapper = styled.div`
   display: flex;
   align-items: center;
-`;
-
-const StyledListItem = styled.li`
-  list-style: none;
-`;
-
-const StyledButton = styled.button`
-  border: none;
-  background-color: white;
-  opacity: ${(props) => props.$needIndex > props.$numberOfNeed && 0.2};
 `;
 
 const StyledLabel = styled.label`
@@ -217,8 +128,4 @@ const StyledLabel = styled.label`
 
 const StyledInput = styled.input`
   display: none;
-`;
-
-const StyledList = styled.ul`
-  display: flex;
 `;
