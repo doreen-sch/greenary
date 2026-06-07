990bd5dc714676b86e09f42c32b508426c54e7b3
Doreen Schwurack <doreen.schwurack@gmx.de>
2026-06-07 22:15:35 +0200

    card & back to top button styling

--- components/PlantCard/index.js
+++ components/PlantCard/index.js
@@ -8,23 +8,38 @@ export default function PlantCard({ plant }) {
 
   return (
     <StyledPlantCard>
-      <Link href={`/${_id}`}>
+      <StyledLink href={`/${_id}`}>
         <StyledPlantCardDiv>
           <StyledImage src={src} alt={name} fill />
         </StyledPlantCardDiv>
         <h2>{name}</h2>
-        <p>{botanicalName}</p>
-      </Link>
+        <StyledH3>{botanicalName}</StyledH3>
+      </StyledLink>
     </StyledPlantCard>
   );
 }
 
 const StyledPlantCard = styled.article`
-  border: 1px solid darkgray;
+  border: 1px solid var(--primary-grey-100);
   border-radius: 10px;
   overflow: hidden;
   flex: 1 1 300px;
-  background-color: var(--secondary-off-white);
+  background-color: white;
+  box-shadow:
+    0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075),
+    0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075),
+    0 16px 16px hsl(0deg 0% 0% / 0.075);
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledH3 = styled.h3`
+  font-weight: 200;
+  font-size: large;
 `;
 
 const StyledPlantCardDiv = styled.div`
