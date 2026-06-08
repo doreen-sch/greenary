5c8a8188acc2b5760c43c766ab7c2c22fa3d9605
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-06-08 13:00:58 +0200

    Revert "positiong and font styling"
    This reverts commit bd093dc4dca006c59b42a7ae3fd97a2e61d4e901.

--- components/Deleteconfirmation/index.js
+++ components/Deleteconfirmation/index.js
@@ -13,7 +13,7 @@ import Button from "../Button";
 
 const StyledContent = styled.div`
   border-radius: 12px;
-  padding: 16px 24px;
+  padding: 16px;
   margin: 16px;
   width: min(320px, calc(100vw - 32px));
   display: flex;
@@ -32,29 +32,28 @@ const StyledContent = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   flex-direction: row;
-  margin-top: 8px;
-  gap: 48px;
-  justify-content: flex-start;
+  gap: 15px;
+  justify-content: space-around;
   align-items: center;
 `;
 
 const PopoverHeadline = styled.h3`
   padding: 0;
   margin: 0;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 800;
-  letter-spacing: -0.02em;
+  letter-spacing: 0.06em;
   color: var(--primary-grey-600);
 `;
 const StyledParagraph = styled.p`
   font-size: 14px;
-  margin-top: 6px;
+  margin: 8px 0px 16px 0px;
   color: var(--primary-grey-500);
 `;
 
 const StyledClose = styled(Close)`
   all: unset;
-  font-size: 12px;
+  font-size: 14px;
   display: inline-flex;
   align-items: center;
   justify-content: center;
